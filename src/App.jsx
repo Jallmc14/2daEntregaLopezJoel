b5e4971b7e4b7eb82591f3f6e0f8533d64ff4cf3
Joel Lopez <jallmc14@gmail.com>
2024-05-01 19:50:19 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar"
+import ItemListContainer from "./components/ItemListContainer"
+import Banner from "./components/Banner"
+import Footer from "./components/Footer"
+import ItemDetailContainer from "./components/ItemDetailContainer"
+import Error404 from "./components/Error404"
+import Carousel from "./components/Carousel";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path={"/"} element={<ItemListContainer />} />
+        <Route path={"/category/:id"} element={<ItemListContainer />} />
+        <Route path={"/item/:id"} element={<ItemDetailContainer />} />
+        <Route path={"*"} element={<Error404 />} />
+      </Routes>
+      <Banner />
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+export default App
